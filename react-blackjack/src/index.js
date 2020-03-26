5ffd9e22fcca21ce52e898bec8ab0f4d12883bf4
MLucky518 <lucky32815@gmail.com>
2020-03-25 20:57:34 -0400

    reducer and store

--- react-blackjack/src/index.js
+++ react-blackjack/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore, applyMiddleWare } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { cardReducer } from "./reducer/index";
 
+const store = createStore(cardReducer, applyMiddleWare(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
-
-
